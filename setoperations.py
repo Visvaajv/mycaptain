d0abd8e6a36de55496823aeb27b6832cc96aa159
Visvaajv <127827107+Visvaajv@users.noreply.github.com>
2024-03-17 13:13:22 +0530

    Add files via upload

---
+++ setoperations.py
@@ -0,0 +1,14 @@
+E = {0, 2, 4, 6, 8}
+N = {1, 2, 3, 4, 5}
+
+union_set = E.union(N)
+print("Union of E and N is", union_set)
+
+intersection_set = E.intersection(N)
+print("Intersection of E and N is", intersection_set)
+
+difference_set = E.difference(N)
+print("Difference of E and N is", difference_set)
+
+symmetric_difference_set = E.symmetric_difference(N)
+print("Symmetric difference of E and N is", symmetric_difference_set)
